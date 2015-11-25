19fe7a20eeec230ea49f739c8736214d47c50e9d
Strets123 <strets123@gmail.com>
2015-11-25 18:33:56 +0000

    image tweaks

--- cbh_chembl_model_extension/models.py
+++ cbh_chembl_model_extension/models.py
@@ -131,7 +131,7 @@ def _mols2imageStream(mols, f, format, size, legend, highlightMatch=None):
     kek = True
     if mols[0].HasProp("_drawingBondsWedged"):
         kek=False
-    image = Draw.MolsToGridImage(mols,molsPerRow=min(len(mols),4),subImgSize=(size * 1.2,size  * 1.2), 
+    image = Draw.MolsToGridImage(mols,molsPerRow=min(len(mols),4),subImgSize=(size,size), 
                                     legends=[ legend for x in mols], kekulize=kek,highlightAtomLists=highlights
  )
     image.save(f, format)
