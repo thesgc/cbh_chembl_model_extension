ace4ea952db5f9e87982df629b3c4d2e427a6994
Strets123 <strets123@gmail.com>
2016-04-06 10:48:42 +0100

    queryset iterator

--- cbh_chembl_model_extension/management/commands/add_ids_to_existing_compounds.py
+++ cbh_chembl_model_extension/management/commands/add_ids_to_existing_compounds.py
@@ -2,23 +2,42 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db import models
 from django.core.paginator import Paginator
 
+import gc
+
+def queryset_iterator(queryset, chunksize=1000):
+    '''''
+    Iterate over a Django Queryset ordered by the primary key
+
+    This method loads a maximum of chunksize (default: 1000) rows in it's
+    memory at the same time while django normally would load all rows in it's
+    memory. Using the iterator() method only causes it to not preload all the
+    classes.
+
+    Note that the implementation of the iterator does not support ordered query sets.
+    '''
+    pk = 0
+    last_pk = queryset.order_by('-pk')[0].pk
+    queryset = queryset.order_by('pk')
+    while pk < last_pk:
+        for row in queryset.filter(pk__gt=pk)[:chunksize]:
+            pk = row.pk
+            yield row
+        gc.collect()
+
+
 def add_ids_to_compounds():
     """Assign project IDs to compounds by saving them"""
     CBHCompoundBatch = models.get_model("cbh_chembl_model_extension", "CBHCompoundBatch")
     Project = models.get_model("cbh_core_model", "Project")
     for p in list(Project.objects.all()):
-        cs = CBHCompoundBatch.objects.filter(project=p).order_by("id")
+        cs = CBHCompoundBatch.objects.filter(project=p).exclude(project_counter=-1).order_by("id")
 
-        paginator = Paginator(cs, 100) # chunks of 1000
+        
+        for obj in queryset_iterator(cs):
 
-        for page in range(1, paginator.num_pages +1):
-            bs = paginator.page(page).object_list
-            for obj in bs:
-                obj.save()
+            obj.save()
             
-            # here you can do what you want with the row
             
-            print "done page %d of %d" % (page ,paginator.num_pages)
         print "done %s" % p.name
 
 
